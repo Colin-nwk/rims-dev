6cd4b4fad541616a3d5c4e9b0a84eb11cba7fd5a
Collins Nwoko C <collinsnwoko05@gmail.com>
2025-12-17 18:01:18 +0100

    feat: Initialize Laravel application with custom API exception handling and v1 API routing.

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -33,6 +34,8 @@ return Application::configure(basePath: dirname(__DIR__))
                     $statusCode = $e->getStatusCode();
                 } elseif ($e instanceof AuthenticationException) {
                     $statusCode = 401;
+                } elseif ($e instanceof AuthorizationException) {
+                    $statusCode = 403;
                 } elseif ($e instanceof ValidationException) {
                     $statusCode = 422;
                 }
