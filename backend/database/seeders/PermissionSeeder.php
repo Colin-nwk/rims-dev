928be6e9ed56495f36647ec26b1bae90c558f49b
Collins Nwoko C <collinsnwoko05@gmail.com>
2025-12-18 12:42:48 +0100

    feat: Add group column to permissions table, update model, and seed initial permission groups.

--- backend/database/seeders/PermissionSeeder.php
+++ backend/database/seeders/PermissionSeeder.php
@@ -14,43 +14,46 @@ class PermissionSeeder extends Seeder
     {
         $permissions = [
             // Staff Management
-            ['name' => 'staff.view', 'description' => 'View staff details'],
-            ['name' => 'staff.create', 'description' => 'Create new staff records'],
-            ['name' => 'staff.edit', 'description' => 'Edit existing staff records'],
-            ['name' => 'staff.delete', 'description' => 'Delete staff records'],
+            ['name' => 'staff.view', 'description' => 'View staff details', 'group' => 'staff'],
+            ['name' => 'staff.create', 'description' => 'Create new staff records', 'group' => 'staff'],
+            ['name' => 'staff.edit', 'description' => 'Edit existing staff records', 'group' => 'staff'],
+            ['name' => 'staff.delete', 'description' => 'Delete staff records', 'group' => 'staff'],
             
             // User Management
-            ['name' => 'user.view', 'description' => 'View system users'],
-            ['name' => 'user.create', 'description' => 'Create new system users'],
-            ['name' => 'user.edit', 'description' => 'Edit system users'],
-            ['name' => 'user.delete', 'description' => 'Delete system users'],
+            ['name' => 'user.view', 'description' => 'View system users', 'group' => 'user'],
+            ['name' => 'user.create', 'description' => 'Create new system users', 'group' => 'user'],
+            ['name' => 'user.edit', 'description' => 'Edit system users', 'group' => 'user'],
+            ['name' => 'user.delete', 'description' => 'Delete system users', 'group' => 'user'],
 
             // Complaint Management
-            ['name' => 'complaint.view', 'description' => 'View complaints'],
-            ['name' => 'complaint.create', 'description' => 'Create complaints'],
-            ['name' => 'complaint.reply', 'description' => 'Reply to complaints'],
-            ['name' => 'complaint.resolve', 'description' => 'Resolve complaints'],
-            ['name' => 'complaint.delete', 'description' => 'Delete complaints'],
+            ['name' => 'complaint.view', 'description' => 'View complaints', 'group' => 'complaint'],
+            ['name' => 'complaint.create', 'description' => 'Create complaints', 'group' => 'complaint'],
+            ['name' => 'complaint.reply', 'description' => 'Reply to complaints', 'group' => 'complaint'],
+            ['name' => 'complaint.resolve', 'description' => 'Resolve complaints', 'group' => 'complaint'],
+            ['name' => 'complaint.delete', 'description' => 'Delete complaints', 'group' => 'complaint'],
 
             // Change Requests
-            ['name' => 'change_request.view', 'description' => 'View change requests'],
-            ['name' => 'change_request.approve', 'description' => 'Approve change requests'],
-            ['name' => 'change_request.reject', 'description' => 'Reject change requests'],
+            ['name' => 'change_request.view', 'description' => 'View change requests', 'group' => 'change_request'],
+            ['name' => 'change_request.approve', 'description' => 'Approve change requests', 'group' => 'change_request'],
+            ['name' => 'change_request.reject', 'description' => 'Reject change requests', 'group' => 'change_request'],
 
             // Access Control (Roles & Permissions)
-            ['name' => 'role.view', 'description' => 'View roles'],
-            ['name' => 'role.create', 'description' => 'Create roles'],
-            ['name' => 'role.edit', 'description' => 'Edit roles'],
-            ['name' => 'role.delete', 'description' => 'Delete roles'],
+            ['name' => 'role.view', 'description' => 'View roles', 'group' => 'role'],
+            ['name' => 'role.create', 'description' => 'Create roles', 'group' => 'role'],
+            ['name' => 'role.edit', 'description' => 'Edit roles', 'group' => 'role'],
+            ['name' => 'role.delete', 'description' => 'Delete roles', 'group' => 'role'],
             
             // Reports
-            ['name' => 'report.view', 'description' => 'View system reports'],
+            ['name' => 'report.view', 'description' => 'View system reports', 'group' => 'report'],
         ];
 
         foreach ($permissions as $permission) {
-            \App\Models\Permission::firstOrCreate(
+            \App\Models\Permission::updateOrCreate(
                 ['name' => $permission['name']],
-                ['description' => $permission['description']]
+                [
+                    'description' => $permission['description'],
+                    'group' => $permission['group']
+                ]
             );
         }
     }
