bf399a61e5c77df8ee556705c77dd3627977da59
Ahire Tersoo David <tersooahire21@gmail.com>
2026-02-03 12:18:58 +0100

    Fix cache clear order in start script

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -6,12 +6,14 @@ php-fpm -D
 
 echo "Clearing configuration cache..."
 php artisan config:clear
-php artisan cache:clear
 php artisan route:clear
 php artisan view:clear
 
 echo "Running database migrations..."
 php artisan migrate --seed --force --no-interaction
+
+echo "Clearing cache after migrations..."
+php artisan cache:clear
 
 echo "Optimizing application..."
 php artisan config:cache
